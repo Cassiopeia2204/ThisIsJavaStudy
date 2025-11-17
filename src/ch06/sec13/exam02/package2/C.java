fd06598e1aaba41962678bcb6b1f2c7490963da1
hojinlee <infohojin@naver.com>
2025-11-17 09:16:28 +0900

    P.266 thisisjava study  commit

---
+++ src/ch06/sec13/exam02/package2/C.java
@@ -0,0 +1,11 @@
+package ch06.sec13.exam02.package2;
+
+import ch06.sec13.exam02.package1.*;
+
+public class C {
+	A a1 = new A(false);
+	
+//	A a2 = new A(2004);   The constructor A(String) is not visible
+	
+//	A a3 = new A("사랑해");   The constructor A(String) is not visible
+}
