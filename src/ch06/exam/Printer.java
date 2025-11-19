e827cf5fb709636f062524c50272523cdd29e62d
yunalee <yunalee2490@gmail.com>
2025-11-19 09:28:10 +0900

    chap06 16~17 exam test commit

---
+++ src/ch06/exam/Printer.java
@@ -0,0 +1,20 @@
+package ch06.exam;
+
+public class Printer {
+	public Printer() {
+		
+	}
+	public static void println(int value){
+		
+	}
+	public static void println(String value){
+			
+	}	
+	public static void println(double value){
+		
+	}
+	public static void println(boolean value){
+		
+	}
+
+}
