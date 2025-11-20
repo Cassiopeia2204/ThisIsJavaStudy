a51b666e64803cce634b40909390a76034f72e74
yunalee <yunalee2490@gmail.com>
2025-11-20 13:41:36 +0900

    chap06 20 exam test commit

--- src/ch06/exam/Account.java
+++ src/ch06/exam/Account.java
@@ -1,23 +1,56 @@
 package ch06.exam;
 
 public class Account {
-	private static final int MAX_BALANCE = 1000000;
-	private static final int MIN_BALANCE = 0;
-	private int balance;
 	
-//	public Account(int balance) {
-//		this.balance = balance;
+	//19번
+//	private static final int MAX_BALANCE = 1000000;
+//	private static final int MIN_BALANCE = 0;
+//	private int balance;
+//	
+////	public Account(int balance) {
+////		this.balance = balance;
+////	}
+//	
+//	public int getBalance() {
+//		return balance;
 //	}
+//	
+//	public void setBalance(int balance) {
+//		if(MIN_BALANCE <= balance && balance <= MAX_BALANCE ) {
+//			this.balance = balance;
+//		}else {
+//			this.balance = getBalance();
+//		}
+//	}
+	//20번
 	
+	public String accNo;
+	public String accName;
+	public int balance;
+	
+	public String getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	public String getAccName() {
+		return accName;
+	}
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
 	public int getBalance() {
 		return balance;
 	}
-	
 	public void setBalance(int balance) {
-		if(MIN_BALANCE <= balance && balance <= MAX_BALANCE ) {
+		if(balance > 0) {
 			this.balance = balance;
 		}else {
 			this.balance = getBalance();
 		}
 	}
+	
+	
+	
 }
---
+++ src/ch06/exam/BankApplication.java
@@ -0,0 +1,59 @@
+package ch06.exam;
+
+import java.util.Scanner;
+
+public class BankApplication {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Account account = new Account();		
+		
+		System.out.println("---------------------------------------------");
+		System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
+		System.out.print("선택> ");
+		int n = sc.nextInt();
+		System.out.println("---------");
+		switch(n) {
+			case 1 :
+				System.out.println("계좌생성");
+				System.out.println("---------");
+				System.out.print("계좌번호: ");
+				String accno = sc.next();
+				account.setAccNo(accno);
+				System.out.print("계좌주: ");
+				String name = sc.next();
+				account.setAccName(name);
+				System.out.print("초기입금액: ");
+				int bal = sc.nextInt();
+				account.setBalance(bal);
+				System.out.println("결과: 계좌가 생성되었습니다.");
+				break;
+			case 2 :
+				System.out.println("계좌목록");
+				System.out.println("---------");
+				System.out.println(account.getAccNo()+"\t"+account.getAccName()+"\t"+account.getBalance());
+				break;
+			case 3 :
+				System.out.println("예금");
+				System.out.println("---------");
+				System.out.print("계좌번호: ");
+				account.setAccNo(sc.next());
+				System.out.print("예금액: ");
+				account.setBalance(sc.nextInt());
+				break;
+			case 4 :
+				System.out.println("출금");
+				System.out.println("---------");
+				System.out.print("계좌번호: ");
+				account.setAccNo(sc.next());
+				System.out.print("출금액: ");
+				n = sc.nextInt();
+				account.setBalance(account.getBalance() - n);
+				break;
+			case 5 :
+				break;
+				
+		}
+			
+	}
+}
