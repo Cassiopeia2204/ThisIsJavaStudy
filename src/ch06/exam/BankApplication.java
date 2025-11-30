b26abbbda6038f48b512c4f579d88c02bf4562a1
yunalee <yunalee2490@gmail.com>
2025-11-30 15:39:12 +0900

    정답이랑 비교

--- src/ch06/exam/BankApplication.java
+++ src/ch06/exam/BankApplication.java
@@ -77,6 +77,41 @@ public class BankApplication {
 			}
 		}
 	}
+	
+//	//예금하기
+//	private static void deposit() {
+//	 System.out.println("--------------");
+//	 System.out.println("예금");
+//	 System.out.println("--------------");
+//	 System.out.print("계좌번호: "); 
+//	String ano = scanner.nextLine();
+//	 System.out.print("예금액: ");
+//	 int money = Integer.parseInt(scanner.nextLine());
+//	 Account account = findAccount(ano);
+//	 if(account == null) {
+//	 System.out.println("결과: 계좌가 없습니다.");
+//	 return;
+//	 }
+//	 account.setBalance(account.getBalance() + money);
+//	 System.out.println("결과: 예금이 성공되었습니다.");
+//	 }
+	
+//	//Account 배열에서 ano와 동일한 Account 객체 찾기
+//	private static Account findAccount(String ano) {
+//	 Account account = null;
+//	 for(int i=0; i<accountArray.length; i++) {
+//		 if(accountArray[i] != null) {
+//			 String dbAno = accountArray[i].getAno();
+//			 if(dbAno.equals(ano)) {
+//				 ccount = accountArray[i];
+//				 break;
+//			 }
+//		 	}
+//	 	}
+//	 	return account;
+//	 }
+	 
+	
 	public static void withdraw() { 
 		System.out.println("출금");
 		System.out.println("---------");
